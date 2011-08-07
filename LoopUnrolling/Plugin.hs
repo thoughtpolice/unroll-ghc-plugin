26a8f383b70ef6e8ede1ca63560fcf5c7a5036cc
Austin Seipp <as@hacks.yi.org>
2011-08-07 00:48:55 -0500

    Update to work with final version of GHC plugins

--- LoopUnrolling/Plugin.hs
+++ LoopUnrolling/Plugin.hs
@@ -9,5 +9,13 @@ plugin = defaultPlugin {
 
 -- Must simplify before hand to get accurate correct recursive loops
 install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
-install _ todos = return $ defaultGentleSimplToDo : unroll : todos
+install _ todos = do 
+    reinitializeGlobals
+    return $ gentle_simpl : unroll : todos
   where unroll = CoreDoPluginPass "Peel/unroll loops" peelUnrollLoopsProgram
+        gentle_simpl = CoreDoSimplify 10 $ SimplMode { sm_phase = InitialPhase
+                                                     , sm_names = ["Gentle"]
+                                                     , sm_rules = False
+                                                     , sm_inline = False
+                                                     , sm_case_case = False
+                                                     , sm_eta_expand = False }
